da0426f94b45dea9d69f2901685db7f715017eca
Shany Praveen <praveen@freshprints.com>
2024-08-31 12:59:44 +0530

    Coded a small movie list app to understand prop drilling and composistion along with reusability

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+import StartRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StartRating color={"red"} size={48} maxRating={5} />
+    <StartRating color={"blue"} size={30} maxRating={10} />
+
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
